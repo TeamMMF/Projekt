e98ae393cfd9e23e99d58a233f034bb2cc4e9883
Filip Sodić <filip.sodic@fer.hr>
2017-11-06 15:47:21 +0100

    refactor fasta sample

---
+++ src/FASTASampleClass.cpp
@@ -0,0 +1,52 @@
+//
+// Created by filip on 06.11.17..
+//
+
+#include <string.h>
+#include <string>
+#include <sstream>
+
+// define a class for sequences in FASTA format
+class FASTASampleClass {
+
+    char *name;
+    char *data;
+
+    uint32_t name_length;
+    uint32_t data_length;
+
+public:
+    FASTASampleClass(uint64_t object_id,
+                     const char *name,
+                     uint32_t name_length,
+                     const char *data,
+                     uint32_t data_length) {
+
+        this->name_length = name_length;
+        this->name = new char[name_length];
+        strncpy(this->name, name, name_length);
+
+        this->data_length = data_length;
+        this->data = new char[data_length];
+        strncpy(this->data, data, data_length);
+    }
+
+    std::__cxx11::string get_description();
+};
+
+std::__cxx11::string FASTASampleClass::get_description() {
+    std::ostringstream oss;
+
+    oss << "Name: ";
+    for (int i = 0; i < name_length; ++i) {
+        oss << name[i];
+    }
+
+    oss << '\n';
+    oss << "Data: ";
+    for (int i = 0; i < data_length; ++i) {
+        oss << data[i];
+    }
+
+    return oss.str();
+}
--- src/fastaSample.cpp
+++ src/fastaSample.cpp
@@ -5,78 +5,24 @@
 #include <iostream>
 #include <vector>
 #include <memory>
-#include <stdint.h>
-#include <string.h>
+
+#include "FASTASampleClass.cpp"
 #include "bioparser/bioparser.hpp"
 
 using namespace std;
 
-// define a class for sequences in FASTA format
-class FASTASampleClas {
-
-	char * name;
-	char * data;
-	string test_string ="asdfasdfasfd";
-
-	uint32_t name_length;
-	uint32_t data_length;
-
-    public:
-        // required signature for the constructor
-        FASTASampleClas(uint64_t object_id,
-            const char* name,
-            uint32_t name_length,
-            const char* data,
-            uint32_t data_length) {
-
-        	   this->name_length = name_length;
-        	   this->name =  new char[name_length];
-        	   strncpy(this->name,name,name_length);
-
-        	   this->data_length = data_length;
-        	   this->data=new char[data_length];
-        	   strncpy(this->data,data,data_length);
-        }
-
-        string get_description();
-        string get_test_string();
-};
-
-string FASTASampleClas :: get_description(){
-	ostringstream oss;
-	
-	oss << "Name: ";
-	for (int i = 0; i < name_length; ++i)
-	{
-		oss << name[i];
-	}
-
-	oss << endl;
-	oss << "Data: ";
-	for (int i = 0; i < data_length; ++i)
-	{
-		oss << data[i];
-	}
-
-	return oss.str();
-}
-
-string FASTASampleClas :: get_test_string(){
-	return test_string;
-}
-
 int main(int argc, char const *argv[])
 {
-	vector<unique_ptr<FASTASampleClas>> fasta_objects;
 
 	string project_root(PROJECT_ROOT);
-	auto fasta_reader = bioparser::createReader<FASTASampleClas, bioparser::FastaReader>(project_root+"src/resources/sample.fasta");
-	
-	fasta_reader->read_objects(fasta_objects, -1);
 
-	for (vector<unique_ptr<FASTASampleClas>>::iterator i = fasta_objects.begin(); i != fasta_objects.end(); ++i)
-	{
-		cout << (**i).get_description() << endl << endl;
+    vector<unique_ptr<FASTASampleClass>> fasta_objects;
+	auto fasta_reader = bioparser::createReader<FASTASampleClass, bioparser::FastaReader>(project_root + "src/resources/sample.fasta");
+	
+	fasta_reader->read_objects(fasta_objects, static_cast<uint64_t>(-1));
+
+	for (auto &fasta_object : fasta_objects) {
+		cout << (*fasta_object).get_description() << endl << endl;
 	}
 
 	return 0;
