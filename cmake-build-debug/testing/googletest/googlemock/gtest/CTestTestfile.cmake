f45d90e206f4ed133eead3cf17e9d8aa87ded8a6
Filip Sodić <filip.sodic@fer.hr>
2017-11-06 18:14:52 +0100

    update gitignore

--- cmake-build-debug/testing/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/filip/CLionProjects/Projekt/testing/googletest/googletest
-# Build directory: /home/filip/CLionProjects/Projekt/cmake-build-debug/testing/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
