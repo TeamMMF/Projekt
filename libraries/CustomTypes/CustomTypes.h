41090bbc1c0e9647d4808899dd3341a9e8183ea5
Mate Paulinovic <mate.paulinovic@fer.hr>
2017-12-08 14:07:00 +0100

    Add struct types

---
+++ libraries/CustomTypes/CustomTypes.h
@@ -0,0 +1,14 @@
+//
+// Created by matep on 08/12/2017.
+//
+
+typedef struct {
+    uint64_t hash;
+    uint16_t index;
+    bool rev;
+} minimizer;
+
+typedef struct {
+    uint16_t index;
+    bool rev;
+} hashEntry;
