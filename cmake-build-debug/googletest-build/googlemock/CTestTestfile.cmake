78b73dbd92fcb82463469ed7c1975998d009a016
Filip Sodić <filip.sodic@fer.hr>
2017-12-08 21:04:59 +0100

    Remove cmake build and add it to gitignore

--- cmake-build-debug/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Faks/5. semestar/Projekt iz programske potpore/Projekt/cmake-build-debug/googletest-src/googlemock
-# Build directory: C:/Faks/5. semestar/Projekt iz programske potpore/Projekt/cmake-build-debug/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
